634b53b866b6df576a6da3ca50c3832b71ace9ea
emyrue <94935633+emyrue@users.noreply.github.com>
2022-08-09 17:56:20 -0600

    Revert "Revert "Successfully display greetings""
    This reverts commit 626ffba780ea06d6304e83ca691c058b7fb58985.

---
+++ src/configureStore.js
@@ -0,0 +1,24 @@
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
+const initialState = {
+  greeting: 'Hi',
+}
+
+function rootReducer(state, action) {
+  console.log(action.type);
+  switch (action.type) {
+    case "GET_GREETING_SUCCESS":
+      return {
+        ...state,
+        greeting: action.payload.greeting,
+      };
+    default:
+      return state;
+  }
+}
+
+export default function configureStore() {
+  const store = createStore(rootReducer, initialState, applyMiddleware(thunk));
+  return store;
+}
