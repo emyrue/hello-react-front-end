626ffba780ea06d6304e83ca691c058b7fb58985
emyrue <94935633+emyrue@users.noreply.github.com>
2022-08-09 17:55:22 -0600

    Revert "Successfully display greetings"
    This reverts commit edbbc50ab4c11c8a57f2a4e067d8bdfae5eb304f.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import logo from './logo.svg';
 import './App.css';
-import Greeting from './components/Greeting';
-import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<h1>Home</h1>} />
-      <Route path="/greet" element={<Greeting greeting="hi" />} />
-    </Routes>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
